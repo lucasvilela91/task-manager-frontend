3b225eaa314a26545b7c9d8000f864d9050b3128
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2025-02-09 20:23:12 -0300

    feat: update TaskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,9 +1,33 @@
+import "./TaskItem.scss";
+
+import { MdDelete } from "react-icons/md";
 const TaskItem = ({ task }) => {
     return (
-        <>
-            <h1>{task.description}</h1>
-            <p>{task.isCompleted ? "Concluída" : "Pendente"}</p>
-        </>
+        <div className="task-item-container">
+            <div className="task-description">
+                <label
+                    className={
+                        task.isCompleted
+                            ? "checkbox-container-completed"
+                            : "checkbox-container"
+                    }
+                >
+                    {task.description}
+                    <input type="checkbox" defaultChecked={task.isCompleted} />
+                    <span
+                        className={
+                            task.isCompleted
+                                ? "checkmark completed"
+                                : "checkmark"
+                        }
+                    ></span>
+                </label>
+            </div>
+
+            <div className="delete">
+                <MdDelete size={18} color="#F97474" />
+            </div>
+        </div>
     );
 };
 
