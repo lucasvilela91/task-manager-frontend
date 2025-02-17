f8c9ab1f0eaddbad994ad347444903b7c1f5ddf1
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2025-02-16 22:49:02 -0300

    feat: add React-Toastify setup

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+import "./AddTask.scss";
+
 import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
+
+import axios from "axios";
 
 const AddTask = () => {
     const [task, setTask] = useState("");
@@ -8,6 +15,19 @@ const AddTask = () => {
     const onChange = (e) => {
         setTask(e.target.value);
     };
+
+    const handleTaskAddition = async () => {
+        try {
+            if (task.length === 0) {
+                return alert.error(
+                    toast.error(
+                        "A tarefa precisa de uma descrição para ser adicionada!"
+                    )
+                );
+            }
+        } catch (error) {}
+    };
+
     return (
         <div className="add-task-container">
             <CustomInput
@@ -15,7 +35,9 @@ const AddTask = () => {
                 value={task}
                 onChange={onChange}
             />
-            <CustomButton>+</CustomButton>
+            <CustomButton onClick={handleTaskAddition}>
+                <FaPlus size={14} color="white" />
+            </CustomButton>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ToastContainer, Zoom } from "react-toastify";
 
 import "./index.scss";
 
@@ -7,6 +8,14 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <ToastContainer
+            position="bottom-center"
+            autoClose={2500}
+            limit={1}
+            pauseOnHover
+            theme="dark"
+            transition={Zoom}
+        />
         <App />
     </StrictMode>
 );
