40b452441c89f39215fc0e3bf01ef8d3351c1863
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2025-02-28 20:49:39 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // import "./App.css";
+import Sidebar from "./components/SideBar";
 import Tasks from "./components/Tasks";
 
+import "./App.scss";
 function App() {
     return (
-        <>
+        <div div className="app-container">
+            <Sidebar />
             <Tasks />
-        </>
+        </div>
     );
 }
 
