36c31adf5cc7ed781cef4e19cfa2d27f7d58a562
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2025-03-04 13:49:43 -0300

    feat: add navigation to home [age in Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 import CustomButton from "../components/CustomButton";
 
 import "./Login.scss";
 import logo from "../assets/images/logo.svg";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSingInClick = () => {
+        navigate("/");
+    };
     return (
         <div className="login-container">
             <img src={logo} alt=" LV developer" />
             <div className="button-container">
-                <CustomButton>Entrar</CustomButton>
+                <CustomButton onClick={handleSingInClick}>Entrar</CustomButton>
             </div>
         </div>
     );
